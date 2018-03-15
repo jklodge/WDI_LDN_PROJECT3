9dfbf2e50c4490aee115eba0258f2b2a60657061
Nic Wilson <nicholaswilson3010@gmail.com>
2018-03-15 15:01:25 +0000

    1 seed added

---
+++ db/seeds.js
@@ -0,0 +1,34 @@
+
+
+{
+  address: '19a, Celia Road, Tuffnell Park, N195ET',
+  image: 'https://www.plumbworld.co.uk/images/ceramica-forli-space-saving-corner-toilet-product-32518-gallery-fA9a-default-H.jpg',
+  description: 'Get your bum in here',
+  facilities: {
+    toilet: true,
+    shower: false,
+    bidet: false,
+    sanitryProducts: false,
+    babyChanging: false
+  },
+  location: {
+    lat: 51.5441512,
+    lng: -0.0986545
+  }
+}
+{
+  address: '19b, Celia Road, Tuffnell Park, N195ET',
+  image: 'https://www.plumbworld.co.uk/images/ceramica-forli-space-saving-corner-toilet-product-32518-gallery-fA9a-default-H.jpg',
+  description: 'Not as good as 19a',
+  facilities: {
+    toilet: true,
+    shower: false,
+    bidet: false,
+    sanitryProducts: false,
+    babyChanging: false
+  },
+  location: {
+    lat: {type: Number},
+    lng: {type: Number}
+  }
+}
