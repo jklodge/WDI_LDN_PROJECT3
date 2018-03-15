240a7611816524f4d2fc2692feb5324acf773a43
Jess Lodge <jessicaklodge@gmail.com>
2018-03-15 14:33:38 +0000

    added to router

--- config/router.js
+++ config/router.js
@@ -1,3 +1,8 @@
-const router = require('express').Router();
 
-module.exports = router;
+
+router.route('/bathrooms/:id')
+  .put(bathrooms.update)
+  .delete(bathrooms.delete);
+
+router.route('/*')
+  .all((req, res) => res.status(404).json({ message: 'Not found' }));
