c0af758f9d1a4106df1e8127e6dbd58ccfc3ad9d
Nic Wilson <nicholaswilson3010@gmail.com>
2018-03-16 14:54:12 +0000

    Index and show routes working on frontend

--- src/app.js
+++ src/app.js
@@ -3,7 +3,16 @@ import '@uirouter/angularjs';
 
 import Router from './config/router';
 
+import BathroomsIndexCtrl from './controllers/bathrooms/index';
+import BathroomsShowCtrl from './controllers/bathrooms/show';
+
+
+import Bathroom from './services/bathroom';
+
 import 'bulma';
 
 angular.module('pooberApp', ['ui.router'])
-  .config(Router);
+  .config(Router)
+  .controller('BathroomsIndexCtrl', BathroomsIndexCtrl)
+  .controller('BathroomsShowCtrl', BathroomsShowCtrl)
+  .service('Bathroom', Bathroom);
