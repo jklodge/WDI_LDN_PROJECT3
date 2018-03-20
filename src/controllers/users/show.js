b9d5492ab788d8763e0e91271294bf1443dee306
Nic Wilson <nicholaswilson3010@gmail.com>
2018-03-20 15:53:23 +0000

    something has gone wrong here

--- src/controllers/users/show.js
+++ src/controllers/users/show.js
@@ -1,13 +1,43 @@
-UsersShowCtrl.$inject = ['User', '$state', '$auth'];
-function UsersShowCtrl(User, $state, $auth) {
+UsersShowCtrl.$inject = ['Bathroom', 'User', '$state', '$auth'];
+function UsersShowCtrl(Bathroom, User, $state, $auth) {
   const vm = this;
   vm.user = {};
+  vm.user.requests = [];
 
   User.findById($auth.getPayload().sub)
     .then(res => {
-      console.log(res);
+      console.log(res.data);
+      res.data.requests = res.data.requests.filter(request => request.user === $auth.getPayload().sub);
       vm.user = res.data;
     });
+
+  Bathroom.find()
+    .then(res => {
+      res.data.forEach(bathroom => {
+        if(bathroom.requests.length > 0 && bathroom.requests[0].user === $auth.getPayload().sub) {
+          // console.log(bathroom.requests[0].user);
+          vm.user.requests.push(bathroom);
+        }
+      });
+      // console.log(vm.user.requests);
+    });
+
+  function acceptRequest(bathroom, request) {
+    Bathroom.acceptRequest(bathroom, request)
+      .then(() => request.status = 'accepted');
+  }
+
+
+
+  function rejectRequest(bathroom, request) {
+    Bathroom.rejectRequest(bathroom, request)
+      .then(() => request.status = 'rejected');
+  }
+
+
+  this.acceptRequest = acceptRequest;
+  this.rejectRequest = rejectRequest;
+
 }
 
 export default UsersShowCtrl;
